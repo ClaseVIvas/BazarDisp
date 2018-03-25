53a95bad655787cc65cc662fc46ab5311ee929d9
PhilipDam2 <feliyepes1@gmail.com>
2018-03-25 14:03:53 +0200

    Creacion Servidor

---
+++ BazarDisp/ServidorBazarDisp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BazarDisp
+{
+    class ServidorBazarDisp
+    {
+        public ServidorBazarDisp()
+        {
+            Inicio();
+        }
+
+        public void Inicio()
+        {
+            // SERVIDOR
+        }
+    }
+}
