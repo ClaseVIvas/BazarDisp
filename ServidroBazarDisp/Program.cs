ee6ab63d68120ec4a9ae239e2b9b3c8c90cf39a4
Cristian <cristianrodriguez@imasdetres.com>
2018-04-23 13:40:45 +0200

    Añadida clase cliente y su implementación en el programa principal de manera superficial

--- ServidroBazarDisp/Program.cs
+++ ServidroBazarDisp/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -16,7 +13,7 @@ namespace ServidroBazarDisp
         static int puerto = 31416;
         static bool ejecucion = true;
         static Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        static string[] dips = new string[] { "One Plus", "Motorola", "Iphone X", "Xiaomi MI MIX 2", "Samsung S9" };
+        static string[] dips = { "One Plus", "Motorola", "Iphone X", "Xiaomi MI MIX 2", "Samsung S9" };
         static List<string> dispositivos = new List<string>();
 
         static void Main(string[] args)
@@ -62,39 +59,28 @@ namespace ServidroBazarDisp
 
         static public void HiloCliente(object socket)
         {
-            //
-            string mensajeCliente;
-            Socket clienteH = (Socket)socket;
-            IPEndPoint ieCliente = (IPEndPoint)clienteH.RemoteEndPoint;
+            Cliente cliente = new Cliente(socket as Socket);
+            IPEndPoint ieCliente = (IPEndPoint)(socket as Socket).RemoteEndPoint;
             //
             Console.WriteLine("\t -- Cliente Conectado {0} en el Puerto {1}", ieCliente.Address, ieCliente.Port);
             foreach (string item in dips)
             {
                 dispositivos.Add(item);
             }
-            //
-            NetworkStream ns = new NetworkStream(clienteH);
-            StreamWriter sw = new StreamWriter(ns);
-            StreamReader sr = new StreamReader(ns);
-
-            sw.WriteLine("BIENVENIDO AL ALMACEN\r\n"+ "Estos Son Los Actuales Dispositivos que estan En Stock\r\n");
-            sw.Flush();
+            cliente.mandarMensaje("BIENVENIDO AL ALMACEN\r\n" + "Estos Son Los Actuales Dispositivos que estan En Stock\r\n");
             try
-            {
-                mensajeCliente = sr.ReadLine();
-                Console.WriteLine(ieCliente.Address + ": " + mensajeCliente);
-                if (mensajeCliente != null)
+            {             
+                Console.WriteLine(ieCliente.Address + ": " + cliente.Mensaje);
+                if (cliente.Mensaje != null)
                 {
-                    /////////
-                    switch (mensajeCliente)
+                    switch (cliente.Mensaje)
                     {
                         case "Dispositivos":
                             int cont = 1;
                             //
                             foreach (String item in dispositivos)
                             {
-                                sw.WriteLine(cont+" -> "+item+"\r\n");
-                                sw.Flush();
+                                cliente.mandarMensaje(cont + " -> " + item + "\r\n");
                                 cont++;
                                 Console.WriteLine(item);
                             }
@@ -106,13 +92,12 @@ namespace ServidroBazarDisp
                             //    sw.WriteLine(item);
                             //}
                             break;
-
                         case "Salir":
-                            sw.WriteLine("HASTA LA PROXIMA " + ieCliente.Address + " --");
+                            cliente.mandarMensaje("HASTA LA PROXIMA " + ieCliente.Address + " --");
                             ejecucion = false;
                             break;
                         default:
-                            sw.WriteLine("Comando No Reconocido");
+                            cliente.mandarMensaje("Comando No Reconocido");
                             break;
                     }
                 }
@@ -129,10 +114,7 @@ namespace ServidroBazarDisp
             }
             // FINALIZAMOS CONEXION
             Console.WriteLine("\t Conexion Finalizada con {0}:{1}", ieCliente.Address, ieCliente.Port);
-            sw.Close();
-            sr.Close();
-            ns.Close();
-            clienteH.Close();
+            cliente.cerrarSesion();
         }
 
     }
